3e8c29b611b0869325a2acfc5b34c2c4d15383ad
Julie Jonak <juliejonak10@gmail.com>
2018-11-27 22:18:52 -0600

    Completed basic server

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+const PORT = 4000;
+
+//endpoints
+
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then((users)=>{
+            res.json(users);
+        })
+        .catch(err => {
+            res
+            .status(500)
+            .json({message: "failed to retrieve users"})
+        });
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    db.findById(id)
+        .then(user => {
+            if(user) {
+                res.json(user);
+            } else{
+                res
+                .status(404)
+                .json({message: "user does not exist"})
+            }
+        })
+        .catch(err => {
+            res
+            .status(500)
+            .json({message: "failed to find that user"})
+        })
+})
+
+
+//listening
+
+server.listen(PORT, () => {
+    console.log(`server GO, port ${PORT}`)
+});
+
